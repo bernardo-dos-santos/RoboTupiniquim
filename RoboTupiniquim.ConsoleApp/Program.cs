3b774d6c2a85b01c73329dd995bdcfe0f4f0f26d
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-26 21:48:14 -0300

    Implementada Lógica Principal do Robô e do método ArrayCircular

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -9,37 +9,71 @@
                 int areaX, areaY;
                 areaX = 5;
                 areaY = 5;
-                int N, S, L, O;
-                 N = 10;
-                 S = 10;
-                 L = 8;
-                 O = 8;
-                int posicaoRoboInicialX = 1;
-                int posicaoRoboInicialY = 2;
-                char direcaoInicial = 'N';
+                char[] direcoes = new char[] {'N', 'L', 'S', 'O' };
+        
+                int posicaoRoboX = 3;
+                int posicaoRoboY = 3;
+                char direcaoInicial = 'L';
 
-                string teste = "EDMEDMEDMEDM";
+                string teste = "MMDMMDMDDM";
 
-                char[] direcoes = new char[teste.Length];
-
+                char[] andar = new char[teste.Length];
+                char letraAtual;
+                for (int caracter = 0; caracter < andar.Length; caracter++)
+                {
+                    letraAtual = teste[caracter];
+                    andar[caracter] = letraAtual;
+                }
+                char direcaoAtual = direcaoInicial;
+                bool direcaocorreta = false;
+                int posicaoArray = 0;
                 for (int i = 0; i < teste.Length; i++)
                 {
-                    if (direcoes[i] == 'E')
+                    for (int j = 0; j < direcoes.Length; j++)
                     {
-
-                    } else if (direcoes[i] == 'D')
-                    {
-
-                    } else if (direcoes[i] == 'M')
-                    {
-
-                    } else
-                        Console.WriteLine("Comando Inválido, Digite apenas as letras E, D e M");
+                        if (direcoes[j] == direcaoAtual)
+                        {
+                            direcaoAtual = direcoes[j];
+                            posicaoArray = j;
+                            direcaocorreta = true;
+                        }
+                    }
+                        if(direcaocorreta == false)
+                        Console.WriteLine("Direção Inicial incorreta, Retornando...");
                         
+                        if (andar[i] == 'E')
+                        {
+                        posicaoArray = ArrayCircular(posicaoArray - 1, direcoes.Length );
+                            direcaoAtual = direcoes[posicaoArray];
+                        } else if (andar[i] == 'D')
+                        {
+                            posicaoArray = ArrayCircular(posicaoArray + 1, direcoes.Length);
+                            direcaoAtual = direcoes[posicaoArray];
+
+                        } else if (andar[i] == 'M')
+                        {
+                            if (direcaoAtual == 'N')
+                            posicaoRoboY += 1;
+                            else if (direcaoAtual == 'S')
+                            posicaoRoboY -= 1;
+                            else if (direcaoAtual == 'L')
+                            posicaoRoboX += 1;
+                            else
+                            posicaoRoboX -= 1;
+                                  
+                        }else
+                        Console.WriteLine("Comando Inválido, Digite apenas as letras E, D e M");
+                    
                 }
 
+                Console.ReadLine();
 
+            }
 
+            static int ArrayCircular(int j, int tamanhoArray)
+            {
+                int teste = ((j % tamanhoArray) + tamanhoArray) % tamanhoArray;
+                return teste;
             }
         }
     }
