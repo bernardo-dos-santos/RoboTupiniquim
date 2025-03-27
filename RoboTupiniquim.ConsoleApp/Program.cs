65d442113ba5215a99f9d442502b7547c4d3a282
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-27 11:44:29 -0300

    Desenvolvimento da Reestruturação do Código, Criação dos métodos SolicitarArea, SolicitarPosicaoInicial, SolicitarDirecoes, ConverterTextoInt, StringParaCharArray, CharArrayParaChar, MovimentacaoRobo

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -6,12 +6,78 @@
         {
             while (true)
             {
+                char[] direcoes = new char[] { 'N', 'L', 'S', 'O' };
+                string areastring = SolicitarArea();
+                int tamanhoX, tamanhoY;
+                char[] areachar = StringParaCharArray(areastring);
+                ConverterTextoInt(out tamanhoX, out tamanhoX, areachar);
+
+                //if (ehnumero == false)
+                //{
+                //Console.WriteLine("Comando Inválido, retornando ao início");
+                //continue;
+                //}
+
+
+
+                string posicoes = SolicitarPosicaoInicial();
+                int posicaoX = 0, posicaoY = 0;
+                char[] posicoeschar = StringParaCharArray(posicoes);
+                ConverterTextoInt(out posicaoX, out posicaoY, posicoeschar);
+                char direcaoAtual = CharArrayParaUmChar(posicoeschar);
+
+
+                string movimentacaoRobo = SolicitarDirecoes();
+                char[] andar = StringParaCharArray(movimentacaoRobo);
+                MovimentacaoRobo(movimentacaoRobo, direcoes, direcaoAtual, andar, posicaoY, posicaoX);
+
+
+
+                Console.ReadLine();
+
+            }
+        }
+
+            public static int ArrayCircular(int j, int tamanhoArray)
+            {
+                int teste = ((j % tamanhoArray) + tamanhoArray) % tamanhoArray;
+                return teste;
+            }
+
+            static string SolicitarArea()
+            {
                 Console.Write("Digite dois numeros, representando a área de pesquisa (Ex: 5 6) ");
                 string areastring = Console.ReadLine()!;
-                char[] areachar = new char[10];
-                int tamanhoX = 0, tamanhoY = 0;
+                return areastring;
+            }
+            static string SolicitarPosicaoInicial()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Digite dois numeros e uma letra, representando a posicao X, Y e Direção incial, respectivamente");
+                Console.WriteLine("Ex: (1 2 L)");
+                string posicoes = Console.ReadLine()!.ToUpper();
+                return posicoes;
+            }
+            static string SolicitarDirecoes()
+            {
+                Console.WriteLine("Digite Comando para mexer o robô, as letras aceitas são: ");
+                Console.WriteLine("E (Virar 90° a esquerda");
+                Console.WriteLine("D (Virar 90° a direita");
+                Console.WriteLine("M (andar para frente na direção colocada)");
+                string movimentacaoRobo = Console.ReadLine()!;
+                return movimentacaoRobo;
+            }
+            static void ConverterTextoInt(out int tamanhoX,out int tamanhoY, char[] areachar )
+            {
                 string stringnumerox, stringnumeroy;
-                bool ehnumero = true;
+                stringnumerox = areachar[0].ToString();
+                stringnumeroy = areachar[1].ToString();
+                int.TryParse(stringnumerox, out tamanhoX);
+                int.TryParse(stringnumeroy, out tamanhoY);
+            }
+            static char[] StringParaCharArray(string areastring)
+            {
+                char[] areachar = new char[areastring.Length];
                 int numerobase = 0;
                 for (int z = 0; z < areastring.Length; z++)
                 {
@@ -23,63 +89,16 @@
                         numerobase++;
                     }
                 }
-                stringnumerox = areachar[0].ToString();
-                stringnumeroy = areachar[1].ToString();
-                ehnumero = int.TryParse(stringnumerox, out tamanhoX);
-                ehnumero = int.TryParse(stringnumeroy, out tamanhoY);
-
-                if (ehnumero == false)
-                {
-                    Console.WriteLine("Comando Inválido, retornando ao início");
-                    continue;
-                }
-                
-               
-                char[] direcoes = new char[] {'N', 'L', 'S', 'O' };
-
-                Console.WriteLine("Digite dois numeros e uma letra, representando a posicao X, Y e Direção incial, respectivamente");
-                Console.WriteLine("Ex: (1 2 L)");
-                string posicoes = Console.ReadLine()!.ToUpper();
-                char[] posicoeschar = new char[10];
-                int posicaoX = 0, posicaoY = 0;
-                string stringPosicaoX, stringPosicaoY;
+                return areachar;
+            }
+            static char CharArrayParaChar(char[] posicoeschar)
+            {
                 char direcaoInicial;
-                bool saoNumeros = true;
-                int numerobase2 = 0;
-                for (int n = 0; n < posicoes.Length; n++)
-                {
-                    char atual = posicoes[n];
-                    if(atual != ' ')
-                    {
-                        posicoeschar[numerobase2] = atual;
-                        numerobase2++;
-                    }
-                }
-                stringPosicaoX = posicoeschar[0].ToString();
-                stringPosicaoY = posicoeschar[1].ToString();
-                saoNumeros = int.TryParse(stringPosicaoX, out posicaoX);
-                saoNumeros = int.TryParse(stringPosicaoY, out posicaoY);
-                if (saoNumeros == false)
-                {
-                    Console.WriteLine("Comando(s) Inválido(s), retornando ao início");
-                    continue;
-                }
                 direcaoInicial = posicoeschar[2];
-
-                Console.WriteLine("Digite Comando para mexer o robô, as letras aceitas são: ");
-                Console.WriteLine("E (Virar 90° a esquerda");
-                Console.WriteLine("D (Virar 90° a direita");
-                Console.WriteLine("M (andar para frente na direção colocada)");
-                string movimentacaoRobo = Console.ReadLine()!;
-
-                char[] andar = new char[movimentacaoRobo.Length];
-                char letraAtual;
-                for (int caracter = 0; caracter < andar.Length; caracter++)
-                {
-                    letraAtual = movimentacaoRobo[caracter];
-                    andar[caracter] = letraAtual;
-                }
-                char direcaoAtual = direcaoInicial;
+                return direcaoInicial;
+            }
+            static void MovimentacaoRobo(string movimentacaoRobo, char[] direcoes, char direcaoAtual, char[] andar, int posicaoY, int posicaoX)
+            {
                 bool direcaocorreta = false;
                 int posicaoArray = 0;
                 for (int i = 0; i < movimentacaoRobo.Length; i++)
@@ -93,42 +112,36 @@
                             direcaocorreta = true;
                         }
                     }
-                        if(direcaocorreta == false)
+                    if (direcaocorreta == false)
                         Console.WriteLine("Direção Inicial incorreta, Retornando...");
-                        
-                        if (andar[i] == 'E')
-                        {
-                        posicaoArray = ArrayCircular(posicaoArray - 1, direcoes.Length );
-                            direcaoAtual = direcoes[posicaoArray];
-                        } else if (andar[i] == 'D')
-                        {
-                            posicaoArray = ArrayCircular(posicaoArray + 1, direcoes.Length);
-                            direcaoAtual = direcoes[posicaoArray];
 
-                        } else if (andar[i] == 'M')
-                        {
-                            if (direcaoAtual == 'N')
+                    if (andar[i] == 'E')
+                    {
+                        posicaoArray = ArrayCircular(posicaoArray - 1, direcoes.Length);
+                        direcaoAtual = direcoes[posicaoArray];
+                    }
+                    else if (andar[i] == 'D')
+                    {
+                        posicaoArray = ArrayCircular(posicaoArray + 1, direcoes.Length);
+                        direcaoAtual = direcoes[posicaoArray];
+
+                    }
+                    else if (andar[i] == 'M')
+                    {
+                        if (direcaoAtual == 'N')
                             posicaoY += 1;
-                            else if (direcaoAtual == 'S')
+                        else if (direcaoAtual == 'S')
                             posicaoY -= 1;
-                            else if (direcaoAtual == 'L')
+                        else if (direcaoAtual == 'L')
                             posicaoX += 1;
-                            else
+                        else
                             posicaoX -= 1;
-                                  
-                        }else
+
+                    }
+                    else
                         Console.WriteLine("Comando Inválido, Digite apenas as letras E, D e M");
-                    
+
                 }
-
-                Console.ReadLine();
-
-            }
-
-            static int ArrayCircular(int j, int tamanhoArray)
-            {
-                int teste = ((j % tamanhoArray) + tamanhoArray) % tamanhoArray;
-                return teste;
             }
         }
     }
